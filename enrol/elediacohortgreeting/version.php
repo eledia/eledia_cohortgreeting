3b71065479a2372e10ce816c51e7fd4ed2f94f3f
Benjamin Wolf <benjamin.wolf@eledia.de>
2016-07-06 17:18:30 +0200

    enrol_elediacohortgreeting - increase version

--- enrol/elediacohortgreeting/version.php
+++ enrol/elediacohortgreeting/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016052300;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016060700;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'enrol_elediacohortgreeting';    // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 60*60;             // run cron every hour by default, it is not out-of-sync often
