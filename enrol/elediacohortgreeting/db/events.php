8770e7ed796393c643ad250dbf5e0b3879f25bea
Benjamin Wolf <benjamin.wolf@eledia.de>
2019-01-03 11:23:11 +0100

    update version and change event callbacks to plugin own handler class

--- enrol/elediacohortgreeting/db/events.php
+++ enrol/elediacohortgreeting/db/events.php
@@ -29,19 +29,19 @@ $observers = array(
 
     array(
         'eventname' => '\core\event\cohort_member_added',
-        'callback' => 'enrol_cohort_handler::member_added',
+        'callback' => 'enrol_elediacohortgreeting_handler::member_added',
         'includefile' => '/enrol/elediacohortgreeting/locallib.php'
     ),
 
     array(
         'eventname' => '\core\event\cohort_member_removed',
-        'callback' => 'enrol_cohort_handler::member_removed',
+        'callback' => 'enrol_elediacohortgreeting_handler::member_removed',
         'includefile' => '/enrol/elediacohortgreeting/locallib.php'
     ),
 
     array(
         'eventname' => '\core\event\cohort_deleted',
-        'callback' => 'enrol_cohort_handler::deleted',
+        'callback' => 'enrol_elediacohortgreeting_handler::deleted',
         'includefile' => '/enrol/elediacohortgreeting/locallib.php'
     ),
 );
